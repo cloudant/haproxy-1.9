rahulagarwal30/haproxy

BUG/MEDIUM: tasks: Make sure we set TASK_QUEUED before adding a task to the rq.
Make sure we set TASK_QUEUED in every case before adding the task to the
run queue. task_wakeup() now checks if either TASK_QUEUED or TASK_RUNNING
is set, and if neither is set, add TASK_QUEUED and effectively add the task
to the runqueue.
No longer use __task_wakeup() anywhere except in task_wakeup(), always use
task_wakeup() instead.
With the old code, process_runnable_task() may re-add a task in the runqueue
without setting the TASK_QUEUED flag, and there were race conditions that could
lead to a task having the TASK_QUEUED flag but not in the runqueue, thus
being unschedulable.

This should be backported to 1.9.

include/proto/task.h
@@ -151,11 +151,12 @@ static inline void task_wakeup(struct task *t, unsigned int f)
 	struct eb_root *root = &task_per_thread[tid].rqueue;
 #endif
 
-	f |= TASK_QUEUED;
-	state = t->state;
-	while (!_HA_ATOMIC_CAS(&t->state, &state, state | f))
-		;
-	if (!(state & TASK_QUEUED))
+	state = _HA_ATOMIC_OR(&t->state, f);
+	while (!(state & (TASK_RUNNING | TASK_QUEUED))) {
+		if (_HA_ATOMIC_CAS(&t->state, &state, state | TASK_QUEUED))
+			break;
+	}
+	if (!(state & (TASK_QUEUED | TASK_RUNNING)))
 		__task_wakeup(t, root);
 }
 

cloudant/haproxy-1.9

(cherry picked from commit b038007ae835ea2ed9b40532d94696a642814e9e)
[wt: s/_HA/HA/; backported as part of the task fix series]
Signed-off-by: Willy Tarreau <w@1wt.eu>

include/proto/task.h
@@ -152,11 +152,12 @@ static inline void task_wakeup(struct task *t, unsigned int f)
 	struct eb_root *root = &task_per_thread[tid].rqueue;
 #endif
 
-	f |= TASK_QUEUED;
-	state = t->state;
-	while (!HA_ATOMIC_CAS(&t->state, &state, state | f))
-		;
-	if (!(state & TASK_QUEUED))
+	state = HA_ATOMIC_OR(&t->state, f);
+	while (!(state & (TASK_RUNNING | TASK_QUEUED))) {
+		if (HA_ATOMIC_CAS(&t->state, &state, state | TASK_QUEUED))
+			break;
+	}
+	if (!(state & (TASK_QUEUED | TASK_RUNNING)))
 		__task_wakeup(t, root);
 }
 